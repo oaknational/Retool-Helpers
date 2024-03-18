f4237c5d7102741dece8455eb7d7aa272abaf64f
Shaquille Hobbs-Daley <shobbsdaley@gmail.com>
2024-03-18 18:30:24 +0000

    chore: update eslint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  ignores: [
+    "**/node_modules/**",
+    "**/lib/**",
+    "**/dist/**",
+    "**/coverage/**",
+    "**/reports/**",
+    "**/public/**",
+    "**/out/**",
+    "**/.husky/**",
+  ],
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+  },
+
+  languageOptions: {
+    sourceType: "module",
+    parser: tseslint.parser,
+    parserOptions: {
+      sourceType: "module",
+      // project: true,
+      tsconfigRootDir: "./configs",
+      project: ["./tsconfig.lint.json"],
+    },
+  },
+
+  files: ["src/**/*.ts"],
+  extends: [eslint.configs.recommended, ...tseslint.configs.strictTypeChecked],
+  rules: {
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
+});
